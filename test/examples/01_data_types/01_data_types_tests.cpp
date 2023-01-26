c36a99e03c0505a84fea9084ab2ee90fd8d8e725
Ronzanl <123336934+Ronzanl@users.noreply.github.com>
2023-01-26 23:48:24 +0000

    First week

--- test/examples/01_data_types/01_data_types_tests.cpp
+++ test/examples/01_data_types/01_data_types_tests.cpp
@@ -1,5 +1,6 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
+#include "char.h"
 
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
@@ -16,6 +17,10 @@ TEST_CASE("Verify Test Configuration", "verification") {
 /*test case add_to_double_2 with 1 as parameter*/
 
 /*test case char test ASCII values */
-
+TEST_CASE("test get char ascii value")
+{
+	REQUIRE(get_char_ascii('A' == 65));
+	REQUIRE(get_char_ascii('a'== 97));
+}
 /*test case string test ASCII values Joe characters are equal to 74, 111, and 101*/
 
