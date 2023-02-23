0259f903f4b2979748f1cacf0537b91c351513e0
Ronzanl <123336934+Ronzanl@users.noreply.github.com>
2023-02-23 22:54:25 +0000

    params

--- src/examples/06_strings/01_create/main.cpp
+++ src/examples/06_strings/01_create/main.cpp
@@ -1,4 +1,14 @@
+#include<iostream>
+#include<string>
+
+using std::cout; using std::string;
+
 int main()
 {
+
+    string lang = "C++";
+    cout<<"Size of: "<<lang.size()<<"\n";
+    lang.append(" is great fo");
+    cout<<"Size of: "<<lang.size()<<"\n";
     return 0;
 }
